172defdd6251741ec2f658e7ebc6602a8fbcd0ec
Pavel <Pavel.Romanov556@yandex.ru>
2016-11-28 01:49:40 +0300

    profile page

---
+++ src/DistantLearning/wwwroot/js/controllers/profileController.js
@@ -0,0 +1,24 @@
+app.controller("profileController", profileController);
+
+function profileController($scope, profileService, consultationService) {
+    $scope.profile = {};
+    $scope.consultations = [];
+
+    profileService.getProfile(function (data) {
+        console.log(data);
+        $scope.profile = data;
+
+        consultationService.getConsultations($scope.profile.id, function (data) {
+            $scope.consultations = data;
+        });
+    });
+
+    $scope.dayOfWeekAsString = function (index) {
+        return ["Пн", "Вт", "Ср", "Чт", "Пт", "Сб", "Вс"][index];
+    };
+
+    $scope.getTime = function (timeString) {
+        var timeTokens = timeString.split(':');
+        return new Date(1970, 0, 1, timeTokens[0], timeTokens[1], timeTokens[2]);
+    };
+}
--- src/DistantLearning/wwwroot/js/services/profileService.js
+++ src/DistantLearning/wwwroot/js/services/profileService.js
@@ -1,4 +1,15 @@
 app.factory("profileService", profileService);
 
 function profileService($http) {
+    return {
+        getProfile: function (callback) {
+            $http.get("/api/profile")
+                .success(function (data) {
+                    callback(data);
+                })
+                .error(function () {
+                    console.error("Problem with getting profile data from the server");
+                });
+        }
+    };
 }
