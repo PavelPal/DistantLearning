263d70643d4d2e2b9139a30d3cb75fc38e68b735
Pavel <Pavel.Romanov556@yandex.ru>
2016-11-27 14:27:27 +0300

    added contollers
    +profile
    +image

---
+++ src/DistantLearning/Controllers/ImageController.cs
@@ -0,0 +1,19 @@
+﻿using Domain.Model;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+
+namespace DistantLearning.Controllers
+{
+    [Authorize]
+    [Route("api/image")]
+    public class ImageController : Controller
+    {
+        private readonly UserManager<User> _userManager;
+
+        public ImageController(UserManager<User> userManager)
+        {
+            _userManager = userManager;
+        }
+    }
+}
---
+++ src/DistantLearning/Controllers/ProfileController.cs
@@ -0,0 +1,28 @@
+﻿using System.Threading.Tasks;
+using DataAccessProvider;
+using Domain.Model;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+
+namespace DistantLearning.Controllers
+{
+    [Authorize]
+    [Route("api/profile")]
+    public class ProfileController : Controller
+    {
+        private readonly UserManager<User> _userManager;
+
+        public ProfileController(UserManager<User> userManager)
+        {
+            _userManager = userManager;
+        }
+
+        [Route("")]
+        [HttpGet]
+        public async Task<User> Profile()
+        {
+            return await _userManager.GetUserAsync(User);
+        }
+    }
+}
