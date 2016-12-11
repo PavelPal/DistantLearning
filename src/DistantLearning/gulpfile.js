328a06efc2bf88405ee12d1e3df6a326117a7ea4
Pavel <Pavel.Romanov556@yandex.ru>
2016-12-12 00:21:38 +0300

    update gulpfile

--- src/DistantLearning/gulpfile.js
+++ src/DistantLearning/gulpfile.js
@@ -12,12 +12,12 @@ var paths = {
     webroot: "wwwroot/"
 };
 
-paths.js = paths.webroot + "js/**/*.js";
+paths.js = paths.webroot + "js/**/**.js";
 paths.minJs = paths.webroot + "js/**/*.min.js";
 paths.css = paths.webroot + "css/**/*.css";
 paths.minCss = paths.webroot + "css/**/*.min.css";
 paths.concatJsDest = paths.webroot + "js/site.min.js";
-paths.concatCssDest = paths.webroot + "css/site.min.css";
+paths.concatCssDest = paths.webroot + "css/main.min.css";
 paths.scss = paths.webroot + "scss/**/*.scss";
 paths.scssDest = paths.webroot + "css/";
 
