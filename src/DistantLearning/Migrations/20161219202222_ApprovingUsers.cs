723a65966e557d13b01e114b648cc0f2d96bbd4c
Pavel <Pavel.Romanov556@yandex.ru>
2016-12-20 00:32:49 +0300

    approvig users
    changed dabase model

---
+++ src/DistantLearning/Migrations/20161219202222_ApprovingUsers.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace distantlearning.Migrations
+{
+    public partial class ApprovingUsers : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<bool>(
+                "IsApproved",
+                "AspNetUsers",
+                nullable: false,
+                defaultValue: false);
+
+            migrationBuilder.AddColumn<bool>(
+                "IsPendingData",
+                "AspNetUsers",
+                nullable: false,
+                defaultValue: false);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                "IsApproved",
+                "AspNetUsers");
+
+            migrationBuilder.DropColumn(
+                "IsPendingData",
+                "AspNetUsers");
+        }
+    }
+}
