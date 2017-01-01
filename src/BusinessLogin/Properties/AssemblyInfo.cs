7e1e6fb8670e6adce77b2ff0ec138e3b7bba07ac
Pavel <Pavel.Romanov556@yandex.ru>
2017-01-02 00:07:28 +0300

    remove unused

--- src/BusinessLogin/Properties/AssemblyInfo.cs
+++
@@ -1,9 +0,0 @@
-﻿using System.Reflection;
-using System.Runtime.InteropServices;
-
-[assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("BusinessLogin")]
-[assembly: AssemblyTrademark("")]
-[assembly: ComVisible(false)]
-[assembly: Guid("3886ab43-bbeb-4d58-9290-822e535b4f21")]
